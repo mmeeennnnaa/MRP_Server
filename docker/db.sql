5d0b1b6a56aaa85cd6398c555b3c4979251a2d67
jj <x.janssen04@hotmail.com>
2025-12-04 23:15:46 +0100

    feat: media tabelle erstellt

--- docker/db.sql
+++ docker/db.sql
@@ -3,3 +3,12 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
     );
+CREATE TABLE IF NOT EXISTS media (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(50), -- 'Movie', 'Series', 'Game'
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    release_year INT,
+    age_restriction INT,
+    creator_id INT REFERENCES users(id) -- Verknüpfung: Welcher User hat es erstellt?
+);
